0d8accadfc27a9ae59b489168436b851cca4fe2f
tony0g <tony0light@gmail.com>
2022-10-29 11:23:18 -0700

    made about me reactive and dynamic

--- components/Section-2.vue
+++ components/Section-2.vue
@@ -2,15 +2,13 @@
 	<div class="w-9/12 bg-base-300 rounded-lg flex flex-col md:flex-row p-5 gap-5">
 		<img class="mask mask-circle w-32" src="/images/tony.jpg">
 		<div class="flex flex-col w-full md:w-2/3">
-			<div ref="miniNav" class="w-full flex flex-row justify-between text-center">
-				<p class="bg-base-100 p-2 rounded-t-lg">Work life</p>
-				<p class="p-2">Personal life</p>
-				<p class="p-2">Coding life</p>
-			</div>
-			<div ref="dialog" class="bg-base-100 p-2">
-				<p>My name is Tony Green. I am a self taught developer with plenty of general work experience. Over my years in the work force I have aquired a classic work ethic, learned to be a part of a team as well as be a leader, and even to work independently.</p>
 
-				<p>I enjoy being challenged and learning new things.</p>
+			<div class="w-full flex flex-row justify-between text-center">
+				<p v-for="obj in textOptions" @click="makeActive(obj.id)" :id="obj.id" class="about-tab p-2 rounded-t-lg hover:cursor-pointer">{{ obj.title }}</p>
+			</div>
+
+			<div class="bg-base-100 p-2 flex flex-col gap-2">
+				<p v-for="line in text">{{ line }}</p>
 			</div>
 			
 		</div>
@@ -18,30 +16,70 @@
 </template>
 
 <script setup>
-	// template refs
-	const miniNav = ref(null)
-	const dialog = ref(null)
 
 	// text to display
 	const text = ref(null)
 
-	const textOptions = {
-		"Work life": [
-			"My name is Tony Green. I am a self taught developer with plenty of general work experience. Over my years in the work force I have aquired a classic work ethic, worked independently and with highly trained teams, and trained others to be effective and efficient.",
-			"I enjoy being challenged and learning new things."
-		],
+	const textOptions = [
 
-		"Coding life": [
-			"I began my coding journey with 'Python Crash Course' by Eric Matthes. Once I finished the book I signed up for a web developer course where I learned Html, CSS, PHP, and Javascript.",
-			"While working through all these steps I had been building projects. From CLI games with python, to web pages with vanilla JS and PHP, to projects using frameworks like Laravel and Vue/Nuxt."
-		],
+		{	
+			id: "work-life",
+			title: "Work life",
+			bodyArray: [
+				"My name is Tony Green. I am a self taught developer with plenty of general work experience. Over my years in the work force I have aquired a classic work ethic, worked independently and with highly trained teams, and trained others to be effective and efficient.",
+				"I enjoy being challenged and learning new things."
+			]
+		},
 
-		"Personal life": [
-			"I am a Washington born, husband and father of two.",
-			"I highly value my time with family and friends. Good conversation is worth it's weight in gold.",
-			"I seek challenges in sports like skydiving and martial arts."
-		],
+		{	
+			id: "coding-life",
+			title: "Coding life",
+			bodyArray: [
+				"I began my coding journey with 'Python Crash Course' by Eric Matthes. Once I finished the book I signed up for a web developer course where I learned Html, CSS, PHP, and Javascript.",
+				"While working through all these steps I had been building projects. From CLI games with python, to web pages with vanilla JS and PHP, to projects using frameworks like Laravel and Vue/Nuxt."
+			]
+		},
+
+		{
+			id: "personal-life",
+			title: "Personal life",
+			bodyArray: [
+				"I am a Washington born, husband and father of two.",
+				"I highly value my time with family and friends. Good conversation is worth it's weight in gold.",
+				"I seek challenges in sports like skydiving and martial arts."
+			]
+		}
+	]
+
+	// 
+	// Utilities
+	// 
+
+	const selectDialog = (id) => {
+		const selection = textOptions.filter(obj => obj.id === id)
+
+		text.value = selection[0].bodyArray
 	}
+
+	const makeActive = (id) => {
+		// remove backgrounds from all
+		let tabs = Array.from(document.querySelectorAll('.about-tab'))
+
+		for (let tab of tabs) {
+			tab.classList.remove('bg-base-100')
+		}
+
+		// add background for active
+		const el = document.getElementById(id)
+		el.classList.add('bg-base-100')
+
+		selectDialog(id)
+		
+	}
+
+	onMounted(() => {
+		makeActive("work-life")
+	})
 
 	
 	
